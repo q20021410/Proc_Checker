f1f239e1b588558fc5c3c73d4c3db34f116cfbde
Aya <webmaster@clsw.kr>
2019-07-06 20:38:59 +0900

    First new release

---
+++ Proc_Checker/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Proc_Checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.nowtime = new System.Windows.Forms.Label();
+            this.Getnowtime = new System.Windows.Forms.Timer(this.components);
+            this.Logview = new System.Windows.Forms.ListView();
+            this.idx = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.stat = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.filepath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rnt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Proccheck = new System.Windows.Forms.Timer(this.components);
+            this.Proc_name = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "현재 시간:";
+            // 
+            // nowtime
+            // 
+            this.nowtime.AutoSize = true;
+            this.nowtime.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.nowtime.Location = new System.Drawing.Point(70, 9);
+            this.nowtime.Name = "nowtime";
+            this.nowtime.Size = new System.Drawing.Size(179, 15);
+            this.nowtime.TabIndex = 1;
+            this.nowtime.Text = "yyyy년 MM월 dd일 tt hh:mm:ss";
+            // 
+            // Getnowtime
+            // 
+            this.Getnowtime.Enabled = true;
+            this.Getnowtime.Tick += new System.EventHandler(this.Getnowtime_Tick);
+            // 
+            // Logview
+            // 
+            this.Logview.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.idx,
+            this.time,
+            this.stat});
+            this.Logview.FullRowSelect = true;
+            this.Logview.GridLines = true;
+            this.Logview.Location = new System.Drawing.Point(13, 85);
+            this.Logview.Name = "Logview";
+            this.Logview.Size = new System.Drawing.Size(386, 188);
+            this.Logview.TabIndex = 2;
+            this.Logview.UseCompatibleStateImageBehavior = false;
+            this.Logview.View = System.Windows.Forms.View.Details;
+            // 
+            // idx
+            // 
+            this.idx.Text = "Idx";
+            this.idx.Width = 47;
+            // 
+            // time
+            // 
+            this.time.Text = "Time";
+            this.time.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.time.Width = 236;
+            // 
+            // stat
+            // 
+            this.stat.Text = "Status";
+            this.stat.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.stat.Width = 73;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(38, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "경로:";
+            // 
+            // filepath
+            // 
+            this.filepath.Location = new System.Drawing.Point(73, 32);
+            this.filepath.Name = "filepath";
+            this.filepath.Size = new System.Drawing.Size(301, 21);
+            this.filepath.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(380, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(19, 20);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "&C";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(354, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 20);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "&Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(9, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "실행주기 :";
+            // 
+            // rnt
+            // 
+            this.rnt.Location = new System.Drawing.Point(73, 59);
+            this.rnt.MaxLength = 3;
+            this.rnt.Name = "rnt";
+            this.rnt.Size = new System.Drawing.Size(28, 21);
+            this.rnt.TabIndex = 8;
+            this.rnt.Text = "123";
+            this.rnt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Rtime);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(100, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "분, 프로세스 명:";
+            // 
+            // Proccheck
+            // 
+            this.Proccheck.Tick += new System.EventHandler(this.Proccheck_Tick);
+            // 
+            // Proc_name
+            // 
+            this.Proc_name.Location = new System.Drawing.Point(195, 58);
+            this.Proc_name.Name = "Proc_name";
+            this.Proc_name.Size = new System.Drawing.Size(153, 21);
+            this.Proc_name.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label6.Location = new System.Drawing.Point(262, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "다음 실행까지: xxx 초";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 277);
+            this.Controls.Add(this.rnt);
+            this.Controls.Add(this.Proc_name);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.filepath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Logview);
+            this.Controls.Add(this.nowtime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Process Checker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label nowtime;
+        private System.Windows.Forms.Timer Getnowtime;
+        private System.Windows.Forms.ListView Logview;
+        private System.Windows.Forms.ColumnHeader idx;
+        private System.Windows.Forms.ColumnHeader time;
+        private System.Windows.Forms.ColumnHeader stat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox filepath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox rnt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer Proccheck;
+        private System.Windows.Forms.TextBox Proc_name;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Proc_Checker/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Proc_Checker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Process Checker (V" + Assembly.GetExecutingAssembly().GetName().Version.ToString() + ")";
+        }
+
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+        [DllImport("kernel32")]
+
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal,
+         int size, string filePath);
+        public static int count = 1;
+        public static string statchk = "Start";
+        public static string ntime = "";
+        public static string crlf = System.Environment.NewLine;
+
+        private void Getnowtime_Tick(object sender, EventArgs e)
+        {
+            nowtime.Text = DateTime.Now.ToString("yyyy년 MM월 dd일 tt hh:mm:ss");
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            filepath.Clear();
+            //String file_path = null;
+            openFileDialog1.InitialDirectory = "C:\\";
+            openFileDialog1.Title = "실행 파일 선택";
+            openFileDialog1.FileName = "OhysCrawler.exe";
+            openFileDialog1.Filter = "실행 파일 (*.exe) | *.exe; | 모든 파일 (*.*) | *.*";
+
+            //openFileDialog의 시작 위치를 C:\\ 로 설정      
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filepath.Text = openFileDialog1.FileName;
+
+                //선택된 파일의 풀 경로를 불러와 저장
+                //filepath.Text = file_path.Split('\\')[file_path.Split('\\').Length - 1];
+                //해당 경로에서 파일명만 추출하여 textBox1에 표시
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WritePrivateProfileString("Proc_Checker", "run_path", filepath.Text, Application.StartupPath + @"\Config.ini");
+            WritePrivateProfileString("Proc_Checker", "runtime", rnt.Text, Application.StartupPath + @"\Config.ini");
+            WritePrivateProfileString("Proc_Checker", "procname", Proc_name.Text, Application.StartupPath + @"\Config.ini");
+
+            StringBuilder run_path = new StringBuilder();
+            StringBuilder run_time = new StringBuilder();
+            StringBuilder run_proc = new StringBuilder();
+
+            Proccheck.Enabled = false;
+            Proccheck.Interval = Convert.ToInt32(rnt.Text) * 60000;
+            Proccheck.Enabled = true;
+            timer1.Enabled = true;
+            label6.Text = Convert.ToString(Convert.ToInt32(rnt.Text) * 60);
+            ntime = Convert.ToString(Convert.ToInt32(rnt.Text) * 60);
+
+            ListViewItem item;
+            count += 1;
+            item = new ListViewItem(count.ToString());
+            item.SubItems.Add(DateTime.Now.ToString("yyyy년 MM월 dd일 tt hh:mm:ss"));
+            item.SubItems.Add("Changed");
+            Logview.Items.Add(item);
+            Logview.EnsureVisible(Logview.Items.Count - 1);
+
+
+        }
+
+        private void Rtime(object sender, KeyPressEventArgs e)
+        {
+            //숫자만 입력되도록 필터링
+            if (!(char.IsDigit(e.KeyChar) || e.KeyChar == Convert.ToChar(Keys.Back))) //숫자와 백스페이스를 제외한 나머지를 바로 처리
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StringBuilder retpath = new StringBuilder();
+            StringBuilder rettime = new StringBuilder();
+            StringBuilder retproc = new StringBuilder();
+
+            //("Form1", "run_path", "test", Application.StartupPath + @"\Config.ini");
+            GetPrivateProfileString("Proc_Checker", "run_path", "", retpath, 128, Application.StartupPath + @"\Config.ini");
+            GetPrivateProfileString("Proc_Checker", "runtime", "", rettime, 32, Application.StartupPath + @"\Config.ini");
+            GetPrivateProfileString("Proc_Checker", "procname", "", retproc, 32, Application.StartupPath + @"\Config.ini");
+
+            filepath.Text = retpath.ToString();
+            rnt.Text = rettime.ToString();
+            Proc_name.Text = retproc.ToString();
+
+            if (rnt.Text.Length <= 0)
+            {
+                ntime = "";
+                timer1.Enabled = false;
+                Proccheck.Enabled = false;
+            }
+            else
+            {
+                ntime = Convert.ToString(Convert.ToInt32(rnt.Text) * 60);
+                timer1.Enabled = true;
+            }
+
+            label6.Text = "다음 실행까지: " + ntime + "초";
+            //label6.Text = Convert.ToString(Convert.ToInt32(rnt.Text) * 60) + "초";
+
+
+            if (rnt.Text.Length > 0)
+            {
+                Proccheck.Interval = Convert.ToInt32(rnt.Text) * 60000;
+                Proccheck.Enabled = true;
+            }
+            else
+            {
+                Proccheck.Interval = 10 * 60000;
+            }
+
+            ListViewItem item;
+
+            item = new ListViewItem(count.ToString());
+            item.SubItems.Add(DateTime.Now.ToString("yyyy년 MM월 dd일 tt hh:mm:ss"));
+            item.SubItems.Add(statchk);
+            Logview.Items.Add(item);
+            Logview.EnsureVisible(Logview.Items.Count - 1);
+
+        }
+
+        private void Proccheck_Tick(object sender, EventArgs e)
+        {
+            StringBuilder retpath = new StringBuilder();
+            GetPrivateProfileString("Proc_Checker", "run_path", "", retpath, 128, Application.StartupPath + @"\Config.ini");
+
+            if (retpath.ToString().Length <= 0)
+            {
+                MessageBox.Show("실행할 프로그램 경로가 지정되어 있지 않습니다."+crlf + "경로를 확인 해 주세요.", "[오류] 실행팔 프로그램이 지정 되어 있지 않습니다.", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+
+                Process[] processList = Process.GetProcessesByName(Proc_name.Text);
+                ListViewItem item;
+                count += 1;
+                if (processList.Length < 1)
+                {
+                    Process.Start(filepath.Text);
+                    statchk = "Proc start";
+                    ntime = Convert.ToString(Convert.ToInt32(rnt.Text) * 60);
+                }
+                else
+                {
+                    statchk = "Okay";
+                    ntime = Convert.ToString(Convert.ToInt32(rnt.Text) * 60);
+                }
+
+                item = new ListViewItem(count.ToString());
+                item.SubItems.Add(DateTime.Now.ToString("yyyy년 MM월 dd일 tt hh:mm:ss"));
+                item.SubItems.Add(statchk);
+                Logview.Items.Add(item);
+                Logview.EnsureVisible(Logview.Items.Count - 1);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(ntime) == 1)
+            {
+               
+                label6.Text = "다음 실행까지: " + ntime + "초";
+            }
+            else
+            {
+                ntime = Convert.ToString(Convert.ToInt32(ntime) - 1);
+                label6.Text = "다음 실행까지: " + ntime + "초";
+            }
+        }
+        
+    }
+}
